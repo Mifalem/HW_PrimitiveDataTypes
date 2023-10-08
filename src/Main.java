1ea8e5a409666de08f57f1649377e72a31d10a6c
My first test <mifalem@gmail.com>
2023-10-08 16:03:11 +0500

    main finished

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        int price = 898;
+        int milesRub = 20;
+        int bonus = price / milesRub;
+                System.out.println(bonus);
+    }
+
 }
